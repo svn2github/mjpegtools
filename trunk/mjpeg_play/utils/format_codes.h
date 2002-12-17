fd74f31e51cd6a4be0d070cae407805650856423
wackston2 <wackston2@3be20b7b-d6de-45d7-9b51-1db703c412de>
2002-12-17 21:54:24 +0000

    Swapped -f 8 and -f 9 so -f 8 stays backward compatible...
    git-svn-id: https://svn.code.sf.net/p/mjpeg/Code@1594 3be20b7b-d6de-45d7-9b51-1db703c412de

--- trunk/mjpeg_play/utils/format_codes.h
+++ trunk/mjpeg_play/utils/format_codes.h
@@ -1,5 +1,5 @@
 /*
-    $Id: format_codes.h,v 1.8 2002-12-11 07:49:54 wackston2 Exp $
+    $Id: format_codes.h,v 1.9 2002-12-17 21:54:24 wackston2 Exp $
 
     Copyright (C) 2001 Andrew Stevens <andrew.stevens@planet-interkom.de>
 
@@ -29,11 +29,11 @@
 #define MPEG_FORMAT_SVCD_NSR 5
 #define MPEG_FORMAT_VCD_STILL 6
 #define MPEG_FORMAT_SVCD_STILL 7
-#define MPEG_FORMAT_DVD      8
-#define MPEG_FORMAT_DVD_NAV 9
+#define MPEG_FORMAT_DVD_NAV 8
+#define MPEG_FORMAT_DVD      9
 
 #define MPEG_FORMAT_FIRST 0
-#define MPEG_FORMAT_LAST MPEG_FORMAT_DVD_NAV
+#define MPEG_FORMAT_LAST MPEG_FORMAT_DVD
 
 #define MPEG_STILLS_FORMAT(x) (x==MPEG_FORMAT_VCD_STILL||x==MPEG_FORMAT_SVCD_STILL)
 #endif /* __FORMAT_CODES_H__ */
