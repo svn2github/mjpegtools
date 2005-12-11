374cbd42463a447c5408e820a8da809788371b34
(no author) <(no author)@3be20b7b-d6de-45d7-9b51-1db703c412de>
2005-12-11 23:50:36 +0000

    This commit was manufactured by cvs2svn to create tag 'Checkpoint-pre2pass'.
    git-svn-id: https://svn.code.sf.net/p/mjpeg/Code@2979 3be20b7b-d6de-45d7-9b51-1db703c412de

---
+++ tags/Checkpoint-pre2pass/mjpeg_play/mpeg2enc/pass1ratectl.cc
@@ -0,0 +1,76 @@
+/* ratectl.c, bitrate control routines  */
+
+/* Copyright (C) 1996, MPEG Software Simulation Group. All Rights Reserved. */
+
+/*
+ * Disclaimer of Warranty
+ *
+ * These software programs are available to the user without any license fee or
+ * royalty on an "as is" basis.  The MPEG Software Simulation Group disclaims
+ * any and all warranties, whether express, implied, or statuary, including any
+ * implied warranties or merchantability or of fitness for a particular
+ * purpose.  In no event shall the copyright-holder be liable for any
+ * incidental, punitive, or consequential damages of any kind whatsoever
+ * arising from the use of these programs.
+ *
+ * This disclaimer of warranty extends to the user of these programs and user's
+ * customers, employees, agents, transferees, successors, and assigns.
+ *
+ * The MPEG Software Simulation Group does not represent or warrant that the
+ * programs furnished hereunder are free of infringement of any third-party
+ * patents.
+ *
+ * Commercial implementations of MPEG-1 and MPEG-2 video, including shareware,
+ * are subject to royalty fees to patent holders.  Many of these patents are
+ * general enough such that they are unavoidable regardless of implementation
+ * design.
+ *
+ */
+
+/* Modifications and enhancements (C) 2000,2001,2002,2003 Andrew Stevens */
+
+/* These modifications are free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ */
+
+#include "config.h"
+#include <cassert>
+#include <math.h>
+#include <limits.h>
+#include "mjpeg_types.h"
+#include "mjpeg_logging.h"
+#include "mpeg2syntaxcodes.h"
+#include "tables.h"
+#include "simd.h"
+#include "fastintfns.h"
+#include "mpeg2encoder.hh"
+#include "picture.hh"
+#include "ratectl.hh"
+#include "quantize.hh"
+
+/* private prototypes */
+
+
+
+
+
+/* 
+ * Local variables:
+ *  c-file-style: "stroustrup"
+ *  tab-width: 4
+ *  indent-tabs-mode: nil
+ * End:
+ */
