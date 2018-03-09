d2f4d1ca1e6cb0f3962b8705fd9158beea20cf4a
sms00 <sms00@3be20b7b-d6de-45d7-9b51-1db703c412de>
2018-03-09 19:14:52 +0000

    obsolete - used/referenced quicktime4linux, movtar and jpeg-mmx
    git-svn-id: https://svn.code.sf.net/p/mjpeg/Code@3425 3be20b7b-d6de-45d7-9b51-1db703c412de

--- trunk/debian-pkg/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mjpegtools package
-#
-0 4	* * *	root	mjpegtools_maintenance
--- trunk/debian-pkg/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mjpegtools):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="mjpegtools" command="/usr/bin/mjpegtools"
